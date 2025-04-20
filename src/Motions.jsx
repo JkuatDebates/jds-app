c6f32da829ddf8da2fe5c9525a92f4837aced044
JkuatDebates <jkuatdebates@gmail.com>
2025-04-20 06:26:48 +0300

    motions JSON update

--- src/Motions.jsx
+++ src/Motions.jsx
@@ -1,14 +1,51 @@
 import { Search } from "lucide-react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 function Motions(){
+    const [displayedMotions, setDisplayedMotions]=useState([]);
+    const searchedMotionsRef=useRef([]);
     const searchRef=useRef(null);
     const motionType=useRef(null);
+    const motionsRef=useRef([]);
     const motionTypes=['','Value', 'Actor', 'Policy', 'Comparative','Narrative'];
 
+    fetch('/motions.json')
+        .then(response=>{
+            !response.ok? console.error('json not loaded')
+            :console.log('json loaded');
+            motionsRef.current=[];
+            return response.json(); 
+        })
+        .then(arr=>arr.forEach(element => {
+            motionsRef.current=[...motionsRef.current,element];
+            //console.log(motionsRef.current)
+        }));
     function searchMotion(){
-        //console.log(searchRef.current.value);
+        //console.log(motionsRef.current.length);
+        searchedMotionsRef.current=motionsRef.current.filter((m)=>m.motion.toLowerCase()
+        .includes(searchRef.current.value.toLowerCase()));
+        setDisplayedMotions(searchedMotionsRef.current);
         searchRef.current.value='';
+        console.log(searchedMotionsRef.current);
+    }
+    function renderMotions(){
+        const rm=shuffle(motionsRef.current);
+        let n=10;
+        const dm=[];
+        while(n>0){
+            dm[n]=rm[n];
+            n--;
+        }
+        console.log(dm);
+        setDisplayedMotions(dm);       
+    }
+    function shuffle(array){
+        const shuffledArray=[...array];
+        for(let i=shuffledArray.length-1;i>0;i--){
+            const random=Math.floor(Math.random()*(i+1));
+            [shuffledArray[i],shuffledArray[random]]=[shuffledArray[random],shuffledArray[i]];
+        }
+        return shuffledArray;
     }
 
     return(
@@ -25,7 +62,17 @@ function Motions(){
             ref={searchRef}/>
             <button style={{padding:'5px'}} onClick={searchMotion}><Search size={'1.5rem'}/></button>
         </div>
-        <div></div>
+        <div>
+            <button onClick={renderMotions}>Refresh</button>
+            <div>
+                {displayedMotions.map((e,i)=>(
+                    <div key={i} className="motionCard"> 
+                    {e.infoslide!==''?<p>{e.infoslide}</p>:''}
+                    {e.motion}
+                    </div>
+                ))}
+            </div>
+        </div>
         </>
     )
 }
