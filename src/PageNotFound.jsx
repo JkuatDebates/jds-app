6cfd61743408b78e956e40416080cbf466a2bfd1
JkuatDebates <jkuatdebates@gmail.com>
2025-04-21 08:47:31 +0300

    Timelines v1

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -4,7 +4,7 @@ function PageNotFound(){
 
     return(
         <div>
-            <p>Seems like yhe page you are looking for does not exist</p>
+            <p>Seems like the page you are looking for does not exist</p>
             <Link to="/" ><button>Go Home</button></Link>
         </div>
     )
--- src/Timelines.jsx
+++ src/Timelines.jsx
@@ -1,8 +1,85 @@
+import { useState } from "react";
+import { FaLocationDot,FaClock, FaMoneyBillWave, FaCreditCard} from "react-icons/fa6";
+import {BsCalendar2DateFill} from "react-icons/bs";
 
 function Timelines(){
+    const today=new Date().toISOString().split('T')[0];
+    const events=[
+        {
+            title: 'Mashujaa Opens XIV',
+            venue: 'Strathmore University',
+            date: '2025-04-13',
+            time:'8 AM EAT',
+            cost:'Ksh.200 per speaker',
+            registrationLink:'',
+            paymentDetails:"",
+            description:"14th edition of Strathmore University's signature debate tournament"
+        },
+        {
+            title: "Sadaqah Li Qaadiya 2025: Debate for Hope's Children's Home",
+            venue: 'DAK Discord(online)',
+            time:'8 AM EAT',
+            cost: "Ksh. 100 (100% donated to children's Home)",
+            date:'2025-05-01',
+            paymentDetails:"Paybill, Business No: 247247, Account No: *140923, Name: Unis Debate Kenya",
+            registrationLink:'https://forms.gle/hmkiKCrFXDE9d2Nt6',
+            description:"Online debate open no novice and opens speakersfor a charitable course"
+        },
+        {
+            title: "Illouwa III",
+            venue: 'TBA',
+            cost: "TBA",
+            time:'8 AM EAT',
+            date:'2025-10-04',
+            paymentDetails:"TBA",
+            registrationLink:'',
+            description:"3rd Edition of JKUAT'S very own debate tournament"
+        },
+    ];
+    
+    const upcomingEvents=events.filter(e=>e.date>=today)
+            .sort((a,b)=>b.date.localeCompare(a.date));
+    const pastEvents=events.filter(e=>e.date<=today)
+                .sort((a,b)=>b.date.localeCompare(a.date));
+
+    function eventList(events){
+        return events.map((e,i)=>(<EventCard key={i} event={e}/>))
+    }
+    
+    return(
+        <div className="timeline">
+            <h1>JDS and Circuit Events</h1>
+            <div className="upcomingEvents">
+                <h3>Upcoming Events</h3>
+                {eventList(upcomingEvents)}
+            </div>
+            <div className="pastEvents">
+                <h3>Past Events</h3>
+                {eventList(pastEvents)}
+            </div>
+        </div>
+    )
+}
+function EventCard({event}){
+    const [details, setDetails]=useState(false);
 
     return(
-        <div>Timelines</div>
-    )
+        <div className="eventCard">
+            <div className="eventHeader" onClick={()=>setDetails(!details)}>
+                <h5><BsCalendar2DateFill size={20} className="icon2"/>{event.date} {event.title}</h5>
+            </div>
+            
+            {details? <div className="eventDetails">
+                <p><strong>Desc:</strong>{event.description}</p>
+                <p><FaLocationDot size={20} className="icon2"/>{event.venue}</p>
+                <p><FaClock size={20} className="icon2"/>{event.time}</p>
+                <p><FaMoneyBillWave size={20} className="icon2"/>{event.cost}</p>
+                {event.paymentDetails!==''?<p><FaCreditCard size={20} className="icon2"/>{event.paymentDetails}</p>:''}
+                {event.registrationLink!==''?<p><a href={event.registrationLink} target="_blank" rel="noreferrer" style={{textDecoration:'none'}}>
+                    <button className="buttonOnBrand">Register Now</button></a></p>:''}
+                </div> :''}
+
+        </div>
+    );
 }
 export default Timelines
